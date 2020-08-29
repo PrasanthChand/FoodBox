f257c5d0f956a935ed5c752f8d9767bb6ee8b3d1
Prasanth <prasanthc70@gmail.com>
2020-08-29 11:56:46 +0530

    removes the old front end codes in same repo as back end

--- src/main/java/com/foodbox/FoodboxApplication.java
+++ src/main/java/com/foodbox/FoodboxApplication.java
@@ -1,9 +1,10 @@
-package com.foodbox.foodbox;
+package com.foodbox;
 
 import java.util.Collections;
 import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,19 +15,36 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @RestController
+@EnableSwagger2
 public class FoodboxApplication extends WebSecurityConfigurerAdapter {
  
+	
+    public static void main(String[] args) {
+        SpringApplication.run(FoodboxApplication.class, args);
+    }
+    
+    
+	@Bean
+	public Docket foodBoxApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.foodBox")).build();
+
+	}
+	
+	
     @GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         return Collections.singletonMap("name", principal.getAttribute("name"));
     }
     
-    public static void main(String[] args) {
-        SpringApplication.run(FoodboxApplication.class, args);
-    }
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	// @formatter:off
