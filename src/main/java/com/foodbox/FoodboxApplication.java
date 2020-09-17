5dac80a1c3027e4d787d7d4801f063ad1b7e4552
Prasanth <prasanthc70@gmail.com>
2020-09-17 23:06:24 +0530

    all functionalities works, pending PCF and Jenkins

--- src/main/java/com/foodbox/FoodboxApplication.java
+++ src/main/java/com/foodbox/FoodboxApplication.java
@@ -1,29 +1,16 @@
 package com.foodbox;
 
-import java.util.Collections;
-import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@RestController
 @EnableSwagger2
-public class FoodboxApplication extends WebSecurityConfigurerAdapter {
+public class FoodboxApplication {
  
 	
     public static void main(String[] args) {
@@ -32,40 +19,12 @@ public class FoodboxApplication extends WebSecurityConfigurerAdapter {
     
     
 	@Bean
-	public Docket foodBoxApi() {
+	public Docket foodboxApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.foodBox")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.foodbox")).build();
 
 	}
-	
-	
-    @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("name", principal.getAttribute("name"));
-    }
-    
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	// @formatter:off
-        http
-            .authorizeRequests(a -> a
-                .antMatchers("/", "/error", "/webjars/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .csrf(c -> c
-                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                )
-            .exceptionHandling(e -> e
-                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                    )
-            .logout(l -> l
-                    .logoutSuccessUrl("/").permitAll()
-                    )
-            .oauth2Login();
-        // @formatter:on
-
-}
 }
 
 
